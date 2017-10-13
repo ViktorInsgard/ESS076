4e529532d85dcee5963ea36c6d61ae2fec6af167
unknown <linsjob@chalmers.se>
2017-10-13 17:28:22 +0200

    FINAL

--- Assignment2/RLS.m
+++ Assignment2/RLS.m
@@ -1,8 +1,10 @@
+clear
+
 % lägg till alla filer: !git add -A
 % spara till git:       !git commit -m "kommentar"
 % upp på nät:           !git push
 
-theta = [.1065; 0.0902; -1.607; .6065]; % real parameters
+theta = [-1.607; .6065; 0.1065; 0.0902]; % real parameters
 
 n = 2; % number of delayed y signals
 m = 2; % number of delayed u signals
@@ -11,17 +13,18 @@ n_t = 1;
 n_s = 2;
 n_r = 2; 
 
-N = 100; % simulation length
+N = 150; % simulation length
 
-lambda = 0.9;
+lambda = 0.8;
 
 theta_hat = zeros(n+m, N);
 phi = zeros(n+m, N);
 P = zeros(n+m, n+m, N);
 P_0 = 0.01*eye(n+m);
-theta_0 = zeros(n+m,1);
+theta_0 = [0; 0; 1 ; 1];
 y = zeros(1,N);
-u = zeros(1,N);
+%u = zeros(1,N);
+u = rand(n+m,N);
 r = ones(1,N);
 
 % controller parameters
@@ -29,6 +32,7 @@ T = zeros(n_t,N);
 S = zeros(n_s,N);
 R = zeros(n_r,N);
 
+% RLS equations
 P(:,:,1) = (1/lambda)*(P_0 - (P_0*(phi(:,1)*phi(:,1)')*P_0) / ...
     (lambda + phi(:,1)'*P_0*phi(:,1)));
 K = P(:,:,1)*phi(:,1);
@@ -39,18 +43,20 @@ T(:,1) = 0.1761 / theta_0(n+1);
 S(1,1) = (-1.3205 - theta_0(1)) / theta_0(n+1);
 S(2,1) = (0.4966 - theta_0(2)) / theta_0(n+1);
 R(1,1) = 1;
-R(2,1) = theta_0(n+2); 
+R(2,1) = theta_0(n+2) / theta_0(n+1); 
+
 
 for k=2:N
+     
     % Adjust y values in phi
     phi(2:n,k) = phi(1:n-1,k-1);
     phi(1,k) = -y(k-1);
     % Adjust u values in phi
     phi(n+2:n+m,k) = phi(n+1:n+m-1,k-1);
     phi(n+1,k) = u(k-1);
-    
-    y(k) = theta'*phi(:,k);
-    
+     
+     y(k) = theta'*phi(:,k);
+     
     P(:,:,k) = (1/lambda)*(P(:,:,k-1) - ...
         (P(:,:,k-1)*phi(:,k-1)*phi(:,k-1)'*P(:,:,k-1)) / ...
         (lambda + phi(:,k-1)'*P(:,:,k-1)*phi(:,k-1)));
@@ -62,9 +68,10 @@ for k=2:N
     S(1,k) = (-1.3205 - theta_hat(1,k)) / theta_hat(n+1,k);
     S(2,k) = (0.4966 - theta_hat(2,k)) / theta_hat(n+1,k);
     R(1,k) = 1;
-    R(2,k) = theta_hat(n+2,k); 
+    R(2,k) = theta_hat(n+2,k)/ theta_hat(n+1,k); 
     
     u(k) = T(1,k)*r(k) - S(1,k)*y(k) - S(2,k)*y(k-1) - R(2,k)*u(k-1);
+    
 end
 
 close all;
